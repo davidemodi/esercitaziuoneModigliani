786f785464e598e2da059b7c12dd0ec720673351
Davide Modigliani <modiglianidavide@gmail.com>
2020-11-10 12:05:54 +0100

    Primo Test File

---
+++ Ciclo1to10.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ciclo1to10;
+
+/**
+ *
+ * @author dani
+ */
+public class Ciclo1to10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i=1; i<11; i++){
+             System.out.println("Il numero è: "+i);
+        }
+    }
+
+}
